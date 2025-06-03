332c79de1318ce55e509d91da4cd16c290f59637
dxnacu <curkandana.den@gmail.com>
2025-06-04 01:37:40 +0300

    app.use('/', express.static('public')); router is deleted

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 // Express setup
 const express = require('express');
 const cors = require('cors');
-const router = express.Router();
-const tripsRoutes = require('./routes/trips');
 const app = express();
 
 // Admin SDK setup
@@ -31,7 +29,7 @@ async function authenticateToken(req, res, next) {
     }
 }
 
-router.get("/api/planned-trips", authenticateToken, async (req, res) => {
+app.get("/api/planned-trips", authenticateToken, async (req, res) => {
     const uid = req.user.uid;
 
     try {
@@ -50,7 +48,7 @@ router.get("/api/planned-trips", authenticateToken, async (req, res) => {
     }
 });
 
-router.post('/api/planned-trips', authenticateToken, async (req, res) => {
+app.post('/api/planned-trips', authenticateToken, async (req, res) => {
     const uid = req.user.uid;
     const tripData = req.body;
 
@@ -100,8 +98,8 @@ app.patch('/api/planned-trips/:tripId/complete', authenticateToken, async (req, 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', tripsRoutes);
-
+app.use('/', express.static('public'));
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
