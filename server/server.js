e58bc18b83cd2b0d1798ef4d55ec2e3246f8516d
dxnacu <curkandana.den@gmail.com>
2025-06-05 18:50:58 +0300

    npm run build is done

--- server/server.js
+++ server/server.js
@@ -122,8 +122,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-// if (!process.env.GOOGLE_CREDENTIALS) {
-//   console.error("Missing GOOGLE_CREDENTIALS env variable");
-//   process.exit(1);
-// }
